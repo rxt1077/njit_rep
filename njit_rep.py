7af14b2199670c84768e29c7739c9b7153371572
Ryan Tolboom <Ryan.Tolboom@monroe.k12.nj.us>
2019-01-22 17:13:41 -0500

    Updated to reputation system

--- njit_rep.py
+++ njit_rep.py
@@ -2,8 +2,11 @@ import random, string
 from bottle import get, post, run, request, template, response, HTTPResponse
 from bottle import abort, redirect, error
 
+# Codes waiting to be validated to register users
 validation_codes = {}
-entries = []
+
+# All our reputation data
+reputations = {}
 
 def create_code(size=16):
     """
@@ -16,7 +19,10 @@ def create_code(size=16):
     return code
 
 @error(401)
-def error404(error):
+def error401(error):
+    """
+    Display a nice 401 page telling people to register
+    """
     return '''
         <div>Access denied</div>
         <div><a href="/register">Please click here to register</a></div>
@@ -38,10 +44,10 @@ def index():
     """
     email = get_email()
     return template('''
-        <div><a href="/create">Create</a></div>
-        % for entry in top_ten:
-            <div>{{entry['author']}}: {{entry['content']}}</div>
-        % end''', top_ten=entries[:10])
+        % for email, reputation in reputations.items():
+            <div>{{email}}: Good - {{reputation["good"]}}, Bad - {{reputation["bad"]}}</div>
+        % end
+    ''', reputations=reputations)
 
 @get('/register')
 def register():
@@ -80,33 +86,9 @@ def validate(code):
         email = validation_codes[code]
         response.set_cookie("account", email, path='/')
         del validation_codes[code]
+        reputations[email] = {'good': 0, 'bad': 0}
         redirect('/')
     else:
-        return "Invalid code!"
-
-@get('/create')
-def create():
-    """
-    Present the form to create new content
-    """
-    email = get_email() 
-    return '''
-        <form action="/create" method="post">
-            What do you have to say? 
-            <input name="content" type="text" maxlength=80 size=80 />
-            <input type="submit" value="Submit" />
-        </form>'''
-
-@post('/create')
-def do_create():
-    """
-    Add the entry to our list
-    """
-    email = get_email()
-    entries.append({
-        'content': request.forms.get('content'),
-        'author': email,
-    })
-    redirect('/')
+        abort(403, "Invalid code!")
 
 run(host='localhost', port=8080, debug=True)
